9be349c4ecdf50a019aff559192123a8ded05e17
annn204 <103918988+annn204@users.noreply.github.com>
2022-04-18 15:55:38 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import HTHeader from './HTHeader/HTHeader'
+import HTMain from './HTMain/HTMain'
+import HTCompany from './HTCompany/HTCompany'
+import HTHoneyStory from'./HTHoneyStory/HTHoneyStory'
+import HTSweetNews from'./HTSweetNews/HTSweetNews'
+import HTLink from './HTLink/HTLinks'
+import HTFooter from './HTFooter/HTFooter'
+
+function App() {
+return (
+     <div className="App">
+          <header className="App-header">
+            <HTHeader/>
+          </header>
+          <nav></nav>
+          <section>
+            <HTMain/>
+            <HTCompany/>
+            <HTHoneyStory/>
+            <HTSweetNews/>
+            <HTLink/>
+          </section>
+          <footer>
+            <HTFooter/>
+          </footer>
+    </div>
+);
+}
+export default App;
